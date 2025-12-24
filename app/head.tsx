f89391251d9648e1426bb66cb8c3aef0c2aa8cfb
Md. Rakib <113634586+0xmdrakib@users.noreply.github.com>
2025-12-24 12:00:32 +0600

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="base:app_id" content="694b33c3c63ad87c690810df" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <title>2048 TX</title>
+      <meta
+        name="description"
+        content="2048 with optional pay-per-move and onchain score saves."
+      />
+      <link rel="icon" href="/icon.png" />
+    </>
+  );
+}
