6d7463301e8fb91fcc632c3850d9a5ed319f3215
Md. Rakib <113634586+0xmdrakib@users.noreply.github.com>
2026-01-06 05:44:03 +0600

    Create weeklySeason.ts

---
+++ lib/server/weeklySeason.ts
@@ -0,0 +1,8 @@
+import "server-only";
+export {
+  getOrInitWeeklyEpoch,
+  getWeekIndex,
+  getWeekBounds,
+  getWeekMeta,
+} from "./leaderboard/weeklySeason";
+export type { WeekMeta } from "./leaderboard/weeklySeason";
